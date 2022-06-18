d00e15739b368d382a11b639ab7982d1524cbd97
BHC-Chicken <tij05078@gmail.com>
2022-06-19 02:34:59 +0900

    update

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator
 from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
-from ..models import Question
+from ..models import Question, Answer, QuestionCount
 
 
 def index(request):
@@ -33,7 +33,39 @@ def index(request):
     return render(request, 'pybo/question_list.html', context)
 
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 def detail(request, question_id):
+    page = request.GET.get('page', '1')
+    so = request.GET.get('so', 'recent')
     question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
+
+    if so == 'recommend':
+        answer_list = Answer.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')\
+            .filter(question=question)
+    else:
+        answer_list = Answer.objects.order_by('-create_date').filter(question=question)
+
+    paginator = Paginator(answer_list, 10)
+    page_obj = paginator.get_page(page)
+
+    ip = get_client_ip(request)
+    cnt = QuestionCount.objects.filter(ip=ip, question=question).count()
+    if cnt == 0:
+        qc = QuestionCount(ip=ip, question=question)
+        qc.save()
+        if question.view_count:
+            question.view_count += 1
+        else:
+            question.view_count = 1
+        question.save()
+
+    context = {'question': question, 'answer_list': page_obj, 'page': page, 'so': so}
     return render(request, 'pybo/question_detail.html', context)
